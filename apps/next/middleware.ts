7cc7d1e3af3f6d69f7b23562fb3b212097a403fe
louis fortrie <louis.fortrie4@gmail.com>
2024-12-23 15:24:57 +0100

    feat: adding stuff

--- apps/next/middleware.ts
+++ apps/next/middleware.ts
@@ -5,6 +5,15 @@ export async function middleware(req: NextRequest) {
   const token = req.cookies.get('access_token')
   console.log('cookie token', token)
   console.log('req.url', req.url)
+
+  if (!token && req.url.endsWith('/')) {
+    const loginUrl = new URL('/login', req.url)
+    return NextResponse.redirect(loginUrl)
+  } else if (token && req.url.endsWith('/')) {
+    const storesListUrl = new URL('/stores-list', req.url)
+    return NextResponse.redirect(storesListUrl)
+  }
+
   if (!token && !req.url.endsWith('/login')) {
     const loginUrl = new URL('/login', req.url)
     return NextResponse.redirect(loginUrl)
